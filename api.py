4ef62e0788e3d132dc231c0fd3845bd34a6ad276
JosephBaria <202180024@psu.palawan.edu.ph>
2023-05-29 01:09:13 +0800

    Flask code added

---
+++ api.py
@@ -0,0 +1,106 @@
+from flask import Flask, make_response, jsonify, request
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+app.config["MYSQL_HOST"] = "localhost"
+app.config["MYSQL_USER"] =  "root"
+app.config["MYSQL_PASSWORD"] =  ""
+app.config["MYSQL_DB"] =  "studentdb"
+app.config["MYSQL_CURSORCLASS"] =  "DictCursor"
+
+mysql = MySQL(app)
+
+@app.route("/")
+def studentdb():
+    return "<p>Student database</p>"
+
+
+def data_fetch(query):
+    cur = mysql.connection.cursor()
+    cur.execute(query)
+    data = cur.fetchall()
+    cur.close()
+    return data
+
+
+
+@app.route("/students", methods=["GET"])
+def get_students():
+    data = data_fetch("""select * from students""")
+    return make_response(jsonify(data), 200)
+
+@app.route("/students/<int:ID>", methods=["GET"])
+def get_student_ID(ID):
+    data = data_fetch("""select * from students where ID = {}""".format(ID))
+    return make_response(jsonify(data), 200)
+
+@app.route("/students/<int:ID>/seat", methods=["GET"])
+def get_seat_by_ID(ID):
+    data = data_fetch("""
+                      select students.FirstNAme, students.LastName, seat.seat_position, seat.seat_no
+                      from students inner join seat 
+                      on students.ID = seat.ID
+                      where students.ID = {}
+                      """.format(ID))
+    return make_response(jsonify(data), 200)
+
+@app.route("/students/<int:ID>/course", methods=["GET"])
+def get_course_by_ID(ID):
+    data = data_fetch("""
+                      select students.FirstNAme, students.LastName, course.Course_name, course.Course_ID
+                      from students inner join course 
+                      on students.ID = course.ID
+                      where students.ID = {}
+                      """.format(ID))
+    return make_response(jsonify(data), 200)
+
+@app.route("/students", methods=["POST"])
+def add_student():
+    cur = mysql.connection.cursor()
+    info = request.get_json()
+    FirstNAme = info["FirstNAme"]
+    LastName = info["LastName"]
+    Address = info["Address"]
+    cur.execute(
+        """Insert into students (FirstNAme, LastName, Address) value (%s, %s, %s)""",
+        (FirstNAme, LastName, Address),
+    )
+    
+    mysql.connection.commit()
+    print("row(s) affected :{}".format(cur.rowcount))
+    rows_affected = cur.rowcount
+    cur.close()
+    return make_response(jsonify({"message": "student added successfully", "rows_affected":rows_affected}),201,)
+
+
+@app.route("/students/<int:ID>", methods = ["PUT"])
+def update_student(ID):
+    cur = mysql.connection.cursor()
+    info = request.get_json()
+    FirstNAme = info["FirstNAme"]
+    LastName = info["LastName"]
+    Address = info["Address"]
+    cur.execute(
+        """update students set FirstNAme = %s, LastName = %s, Address = %s where ID = %s""",
+        (FirstNAme, LastName, Address, ID),
+    )
+    
+    mysql.connection.commit()
+    print("row(s) affected :{}".format(cur.rowcount))
+    rows_affected = cur.rowcount
+    cur.close()
+    return make_response(jsonify({"message": "student updated successfully", "rows_affected":rows_affected}),200,)
+
+@app.route("/students/<int:ID>", methods = ["DELETE"])
+def delete_student(ID):
+    cur = mysql.connection.cursor()
+    cur.execute(""" delete from students where ID = %s """,(ID,))
+    mysql.connection.commit()
+    rows_affected = cur.rowcount
+    cur.close()
+    return make_response(jsonify({"message": "student delete successfully", "rows_affected":rows_affected}),200,)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
